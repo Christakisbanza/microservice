fc61418157a6933e1cbaf49e391c113fa01444a0
Christakis <banzachristakis@gmail.com>
2024-12-11 15:19:37 -0300

    base configuration done in msCloudGateway

--- mscloudgateway/mscloudgateway/src/main/java/com/gateway/mscloudgateway/MscloudgatewayApplication.java
+++ mscloudgateway/mscloudgateway/src/main/java/com/gateway/mscloudgateway/MscloudgatewayApplication.java
@@ -2,12 +2,25 @@ package com.gateway.mscloudgateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class MscloudgatewayApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MscloudgatewayApplication.class, args);
+	}
+
+	@Bean
+	public RouteLocator routeLocator(RouteLocatorBuilder routeLocatorBuilder){
+		return routeLocatorBuilder
+				.routes()
+				.route(r -> r.path("/clients/**").uri("lb://msclients"))
+				.build();
 	}
 
 }
